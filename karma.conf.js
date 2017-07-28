0ecb240ddeb4272f2c318add0dc492b516a5cf8a
Chris Navarro <cmnavarr@illinois.edu>
2017-07-28 12:55:02 -0500

    Fixed missing import and unnecessary css imports, removed line from babelrc to fix build and updated karma to run chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,18 @@ var path = require("path");
 module.exports = function (config) {
 	config.set({
 		//browsers: ["Chrome", "Firefox"], //run in Chrome
-		browsers: ["PhantomJS"], //run headless for bamboo 
+		browsers: ["PhantomJS", "ChromeHeadless"], //run headless for bamboo
+		customLaunchers: {
+  		ChromeHeadless: {
+  			base: 'Chrome',
+  			flags: [
+  			'--headless',
+  			'--disable-gpu',
+  			// Without a remote debugging port, Google Chrome exits immediately.
+  			'--remote-debugging-port=9222',
+  			]
+  		}
+		},
 		singleRun: true,
 		colors: true,
 		basePath: "",
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {IndexLink, Link} from "react-router";
-import "material-components-web/dist/material-components-web.min.css";
 import {Cell, Grid, Title, Textfield, Button, Caption, Body1, Subheading2} from "react-mdc-web";
 
 class Header extends Component {
