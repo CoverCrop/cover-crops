8922fe5bf9bd64314efcca8fbb0ba29171a2f1b0
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2017-09-08 11:44:35 -0500

    1. Added CLU layer 2. Added drop marker on click functionality 3. Fixed issue with map view changing with window resize

--- src/components/MapCC.jsx
+++ src/components/MapCC.jsx
@@ -1,6 +1,4 @@
 import React, {Component} from "react";
-import {Button, Textfield, Body1, Grid, Cell} from "react-mdc-web";
-import CoordinateFieldCC from "./CoordinateFieldCC";
 let ol = require('openlayers');
 require("openlayers/css/ol.css");
 
@@ -8,8 +6,25 @@ class MapCC extends Component {
 
 	constructor(props) {
 		super(props);
-		this.defaultCenter = ol.proj.fromLonLat([-88.241919, 40.116726]); //[-9822960.723442, 4882854.694628];
-		this.defaultZoom = 12;
+		this.defaultCenter = ol.proj.fromLonLat([-88.213511, 40.121686]); //[-9822960.723442, 4882854.694628];
+		this.defaultZoom = 16;
+
+		this.iconStyle = new ol.style.Style({
+			image: new ol.style.Icon(({
+				anchor: [0.5, 46],
+				anchorXUnits: 'fraction',
+				anchorYUnits: 'pixels',
+				opacity: 0.80,
+				src: "../images/map-marker.png"
+			}))
+		});
+
+		this.markerSource = new ol.source.Vector({
+			features: []
+		});
+		this.marker = new ol.layer.Vector({
+			source: this.markerSource
+		});
 
 		this.state = {
 			map: new ol.Map({
@@ -26,7 +41,8 @@ class MapCC extends Component {
 							params: {'LAYERS': 'covercrop:clu', 'TILED': true},
 							serverType: 'geoserver'
 						})
-					})
+					}),
+					this.marker
 				],
 				view: new ol.View({
 					center: this.defaultCenter,
@@ -40,16 +56,38 @@ class MapCC extends Component {
 	}
 
 	handleClick(e) {
-		console.log(e);
+		this.dropMarker(e.coordinate);
+		console.log(ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326'));
 	}
 
-	componentDidMount(e){
-		this.state.map.setTarget(this.props.mapId);
+	dropMarker(coordinate) {
+
+		let iconFeature = new ol.Feature({
+			geometry: new ol.geom.Point(coordinate)
+		});
+		iconFeature.setStyle(this.iconStyle);
+
+		this.markerSource.clear();
+		this.markerSource.addFeature(iconFeature);
+	}
+
+	componentDidMount(e) {
+		this.state.map.setTarget(this.props.mapId); // Set target for map
+		this.state.map.on("click", this.handleClick); // Set on click event handler
+		this.dropMarker(this.defaultCenter); // Drop default marker
 	}
 
 	render(){
+		const mapStyle = {
+			width: 900,
+			height: 675,
+			backgroundColor: '#ebebeb'
+		};
+
 		return(
-			<div onClick={this.handleClick} id={this.props.mapId}/>
+			<div>
+				<div style={mapStyle} id={this.props.mapId}/>
+			</div>
 		)
 	}
 
