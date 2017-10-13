a03369933e97353b6958c554763fa9dbdfe2cc2e
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2017-10-13 11:52:30 -0500

    1. Changed result dataset ID to get new JSON result 2. Now checking for errors at server side and providing some basic feedback to user 3. Storing parameters for cases with and without cover crops 4. Code cleanup

--- src/components/RunSimluationCC.jsx
+++ src/components/RunSimluationCC.jsx
@@ -13,6 +13,12 @@ class RunSimulationCC extends Component {
 		this.runSimulation = this.runSimulation.bind(this);
 		this.handleStartDateChange = this.handleStartDateChange.bind(this);
 		this.handleEndDateChange = this.handleEndDateChange.bind(this);
+		this.parameters = {
+			soilWithCoverCrop: "d19c89f7-ae89-4d3a-b0de-393fea0b6454", // ILLT1101.SQX
+			modelWithCoverCrop: "f405c6c1-7180-4f03-be91-42b82550085c", // DSSBatch.v46
+			soilWithoutCoverCrop: "5d1399f9-7068-4bd7-8cf0-aceb52febbf4", // ILWO1101.SQX
+			modelWithoutCoverCrop: "3813396e-5ab0-418c-8e8f-34eda231fadf" // DSSBatch-nocover.v46
+		};
 
 		this.state = {
 			executionId: "",
@@ -54,12 +60,9 @@ class RunSimulationCC extends Component {
 				"dcceaa12-2bc6-4591-8e14-026c3bad64fd" : this.props.state.longitude
 			},
 			"datasets" : {
-				// Without cover crop
-				// "323c6613-4037-476c-9b9c-f51ba0940eaf" : "5d1399f9-7068-4bd7-8cf0-aceb52febbf4", // ILWO1101.SQX
-				// "7db036bf-019f-4c01-e58d-14635f6f799d" : "3813396e-5ab0-418c-8e8f-34eda231fadf" // DSSBatch-nocover.v46
 				// With cover crop
-				"323c6613-4037-476c-9b9c-f51ba0940eaf" : "d19c89f7-ae89-4d3a-b0de-393fea0b6454", // ILLT1101.SQX
-				"7db036bf-019f-4c01-e58d-14635f6f799d" : "f405c6c1-7180-4f03-be91-42b82550085c" // DSSBatch.v46
+				"323c6613-4037-476c-9b9c-f51ba0940eaf" : this.parameters.soilWithCoverCrop,
+				"7db036bf-019f-4c01-e58d-14635f6f799d" : this.parameters.modelWithCoverCrop
 			}
 		};
 
@@ -88,76 +91,86 @@ class RunSimulationCC extends Component {
 		console.log("Execution response: " + executionResponse);
 
 		const analysisResult = await executionResponse.json();
-		const datasetResultGUID = analysisResult.datasets["8ba31e5e-f343-4c9b-c41c-656b9f66b417"];
+		const datasetResultGUID = analysisResult.datasets["2623a440-1f16-4110-83c4-5ebf39cb0e35"];
 
 		console.log("result dataset = " + datasetResultGUID);
 
-		// Get - Result Dataset
-		const response = await fetch(datawolfURL + "/datasets/" + datasetResultGUID, {
-			method: 'GET',
-			headers: headers,
-		});
-
-		const resultDataset = await response.json();
-		let fileDescriptorGUID = -1;
-
-		for (let i=0; i < resultDataset.fileDescriptors.length; i++) {
-			if (resultDataset.fileDescriptors[i].filename === "PlantGro.OUT") {
-				fileDescriptorGUID = resultDataset.fileDescriptors[i].id;
-				break;
-			}
-		}
-
-		// Get - Result File Download
-		const fileDownloadResponse = await fetch(datawolfURL + "/datasets/" + datasetResultGUID + "/" + fileDescriptorGUID + "/file",
-			{
+		if (datasetResultGUID !== "ERROR") {
+			// Get - Result Dataset
+			const response = await fetch(datawolfURL + "/datasets/" + datasetResultGUID, {
 				method: 'GET',
 				headers: headers,
 			});
 
-		const resultFile = await fileDownloadResponse.text();
-		console.log("result file = " + resultFile);
+			const resultDataset = await response.json();
+			let fileDescriptorGUID = -1;
 
-		this.setState({
-			resultText: resultFile,
-			executionId: executionGUID
-		});
+			for (let i = 0; i < resultDataset.fileDescriptors.length; i++) {
+				if (resultDataset.fileDescriptors[i].filename === "output.json") {
+					fileDescriptorGUID = resultDataset.fileDescriptors[i].id;
+					break;
+				}
+			}
 
-		console.log(analysisResult);
+			// Get - Result File Download
+			const fileDownloadResponse = await fetch(datawolfURL + "/datasets/" + datasetResultGUID + "/" + fileDescriptorGUID + "/file",
+				{
+					method: 'GET',
+					headers: headers,
+				});
 
-		status = "COMPLETED";
-		this.setState({
-			simulationStatus: status,
-			runSimulationButtonDisabled: false
-		});
+			const resultFile = await fileDownloadResponse.json();
+			console.log("Result JSON: ");
+			console.log(resultFile);
 
-		// Update status
-		cardData = {
-			cardTitle: this.props.state.cards[1].cardTitle,
-			cardSubtitle: "Status: " + status
-		};
-		this.props.handleCardChange(1, 1, cardData);
+			this.setState({
+				resultText: resultFile,
+				executionId: executionGUID
+			});
 
-		if (this.state.executionId !== "") {
+			console.log(analysisResult);
+
+			status = "COMPLETED";
+			this.setState({
+				simulationStatus: status,
+				runSimulationButtonDisabled: false
+			});
+
+			// Update status
 			cardData = {
-				cardTitle: "Completed Simulation",
-				cardSubtitle: "Execution ID: " + this.state.executionId
+				cardTitle: this.props.state.cards[1].cardTitle,
+				cardSubtitle: "Status: " + status
 			};
-			this.props.handleResults(this.state.executionId, this.state.resultText);
-			this.props.handleCardChange(1, 2, cardData);
+			this.props.handleCardChange(1, 1, cardData);
+
+			if (this.state.executionId !== "") {
+				cardData = {
+					cardTitle: "Completed Simulation",
+					cardSubtitle: "Execution ID: " + this.state.executionId
+				};
+				this.props.handleResults(this.state.executionId, this.state.resultText);
+				this.props.handleCardChange(1, 2, cardData);
+			}
+			else {
+				console.log("Execution ID wasn't generated.");
+			}
 		}
 		else {
-			console.log("Execution ID wasn't generated.");
-		}
+			status = "ERROR";
+			console.log("Error occurred while running workflow");
+			this.setState({
+				simulationStatus: status,
+				runSimulationButtonDisabled: false
+			});
 
-		// const datasetGet = await fetch(datawolfURL + '/workflows', {
-		// 	method: 'GET',
-		// 	headers: headers
-		// });
-		//
-		// const datasets = await datasetGet.json();
-		//
-		// console.log(datasets);
+			// Update status
+			cardData = {
+				cardTitle: this.props.state.cards[1].cardTitle,
+				cardSubtitle: "Status: " + status
+			};
+			this.props.handleCardChange(1, 1, cardData);
+
+		}
 	}
 
 	handleStartDateChange(date) {
