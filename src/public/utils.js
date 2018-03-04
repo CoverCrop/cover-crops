efff087d4304d03d9bbc587885a947abbcb410b1
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2018-03-03 22:29:49 -0600

    Added credentials to request for checking user authentication.

--- src/public/utils.js
+++ src/public/utils.js
@@ -7,15 +7,14 @@ import {datawolfURL} from "../datawolf.config";
 export async function checkAuthentication() {
 
 	let personId = localStorage.getItem("personId");
-	let token = ""; // TODO: Get token here
 
 	return await fetch(datawolfURL + "/persons/" + personId, {
 		method: 'GET',
 		headers: {
-			"Authorization": token,
 			"Content-Type": "application/json",
 			"Access-Control-Origin": "http://localhost:3000"
-		}
+		},
+		credentials: "include"
 	});
 }
 
