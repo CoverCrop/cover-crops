ded86baa68f7ed3f2e8661bc41e11e7bdffbeb20
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2018-03-29 17:16:34 -0500

    Added logic for registration and displaying related response status messages.

--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {
+	Body1,
 	Button,
 	Card,
 	CardActions,
@@ -14,7 +15,7 @@ import {
 } from "react-mdc-web";
 import Header from "./Header";
 import Footer from "./Footer";
-import {connect} from "react-redux";
+import {datawolfURL} from "../datawolf.config";
 
 class RegistrationPage extends Component {
 
@@ -28,7 +29,8 @@ class RegistrationPage extends Component {
 			password: "",
 			passwordConfirm: "",
 			arePasswordsMatching: false,
-			hasMinimumPasswordLength: false
+			hasMinimumPasswordLength: false,
+			statusText: ""
 		};
 
 		this.handleRegistration = this.handleRegistration.bind(this);
@@ -38,6 +40,76 @@ class RegistrationPage extends Component {
 	}
 
 	handleRegistration = async event => {
+		event.preventDefault();
+		this.setState({statusText: ""});
+		let genericErrorMessage = "An error occurred while trying to register your account. Please try again later.";
+		let userNotActiveMessage = "Registration is successful, but your account is not active yet. " +
+			"Please contact an administrator.";
+
+		try {
+			let createPersonResponse = await fetch(datawolfURL + "/persons?" +
+				"firstName=" + this.state.firstName +
+				"&lastName=" + this.state.lastName +
+				"&email=" + this.state.email,
+				{
+					method: 'POST',
+					headers: {
+						"Content-Type": "text/plain",
+						"Access-Control-Origin": "http://localhost:3000"
+					}
+				});
+
+			if (createPersonResponse.status === 200 || createPersonResponse.status === 204) {
+
+				let personId = await createPersonResponse.text();
+
+				let createAccountResponse = await fetch(datawolfURL + "/login?" +
+					"email=" + this.state.email +
+					"&password=" + this.state.password,
+					{
+						method: 'POST',
+						headers: {
+							"Content-Type": "text/plain",
+							"Access-Control-Origin": "http://localhost:3000"
+						}
+					});
+
+				// Account creation successful
+				if (createAccountResponse.status === 200) {
+					let responseText = await createAccountResponse.text();
+
+					// User needs to be activated
+					if(responseText === "Not Active") {
+						console.log("User account not active.");
+						this.setState({statusText: userNotActiveMessage});
+					}
+					// User is already active
+					else {
+						console.log("User account active.");
+						sessionStorage.setItem("personId", personId); // Store person ID in session storage for future use
+					}
+				}
+				// Account creation unsuccessful. User already exists.
+				else if (createAccountResponse.status === 500){
+					let responseText = await createAccountResponse.text();
+					console.log("Status Text: " + responseText);
+					this.setState({statusText: responseText});
+				}
+				else {
+					console.log("Registration Step 2 Status: " + createAccountResponse.status);
+					let responseText = await createAccountResponse.text();
+					this.setState({statusText: genericErrorMessage});
+				}
+			}
+			else {
+				console.log("Registration Step 1 Status: " + createPersonResponse.status);
+				this.setState({statusText: genericErrorMessage});
+			}
+		}
+		catch (error) {
+			console.error("Error: " + error);
+			this.setState({statusText: genericErrorMessage});
+		}
 	};
 
 	handleReset = () => {
@@ -48,7 +120,8 @@ class RegistrationPage extends Component {
 			password: "",
 			passwordConfirm: "",
 			arePasswordsMatching: false,
-			hasMinimumPasswordLength: false
+			hasMinimumPasswordLength: false,
+			statusText: ""
 		});
 	};
 
@@ -62,14 +135,12 @@ class RegistrationPage extends Component {
 	};
 
 	verifyPasswordLength = () => {
-		console.log(this.state.password.length);
 		if (this.state.password.length >= 6 ) {
 			this.setState({hasMinimumPasswordLength: true});
 		}
 		else {
 			this.setState({hasMinimumPasswordLength: false});
 		}
-		console.log(this.state.hasMinimumPasswordLength);
 	};
 
 	validateRegistrationForm() {
@@ -91,71 +162,74 @@ class RegistrationPage extends Component {
 						<Cell col={4}/>
 						<Cell col={3}>
 							<div>
-								<Card className="registration">
-									<CardHeader>
-										<Title>Create an account</Title>
-									</CardHeader>
-									<CardText>
-										<div><Textfield autoFocus floatingLabel="First name"
-														required
-														value={this.state.firstName}
-														size="40"
-														onChange={({target: {value: firstName}}) => {
-															this.setState({firstName: firstName})
-														}}/></div>
-										<div><Textfield floatingLabel="Last name" value={this.state.lastName}
-														required
-														size="40"
-														onChange={({target: {value: lastName}}) => {
-															this.setState({lastName: lastName})
-														}}/></div>
-										<div><Textfield floatingLabel="Email ID" value={this.state.email}
-														required
-														size="40"
-														type="email"
-														helptext="Please enter a valid email address."
-														helptextValidation
-														onChange={({target: {value: email}}) => {
-															this.setState({email: email})
-														}}/></div>
-										<div><Textfield floatingLabel="Password" type="password"
-														value={this.state.password}
-														required
-														size="40"
-														useInvalidProp
-														invalid={!this.state.hasMinimumPasswordLength}
-														helptext="Your password must be contain at least 6 letters."
-														helptextValidation
-														onChange={({target: {value: password}}) => {
-															this.setState({password: password})
-														}}
-														onKeyUp={this.verifyPasswordLength}/></div>
-										<div><Textfield floatingLabel="Confirm Password" type="password"
-														required
-														value={this.state.passwordConfirm}
-														size="40"
-														useInvalidProp
-														invalid={!this.state.arePasswordsMatching}
-														helptext={"Passwords do not match."}
-														helptextValidation
-														onChange={({target: {value: passwordConfirm}}) => {
-															this.setState({passwordConfirm: passwordConfirm})
-														}}
-														onKeyUp={this.verifyPasswordMatch}/></div>
-									</CardText>
-									<CardActions>
-										<Button
-											type="submit"
-											raised
-											onClick={this.handleRegistration}
-											disabled={!this.validateRegistrationForm()}>Register
-										</Button>
-										<Button
-											raised
-											onClick={this.handleReset}>Clear
-										</Button>
-									</CardActions>
-								</Card>
+								<form>
+									<Card className="registration">
+										<CardHeader>
+											<Title>Create an account</Title>
+										</CardHeader>
+										<CardText>
+											<div><Textfield autoFocus floatingLabel="First name"
+															required
+															value={this.state.firstName}
+															size="40"
+															onChange={({target: {value: firstName}}) => {
+																this.setState({firstName: firstName})
+															}}/></div>
+											<div><Textfield floatingLabel="Last name" value={this.state.lastName}
+															required
+															size="40"
+															onChange={({target: {value: lastName}}) => {
+																this.setState({lastName: lastName})
+															}}/></div>
+											<div><Textfield floatingLabel="Email ID" value={this.state.email}
+															required
+															size="40"
+															type="email"
+															helptext="Please enter a valid email address."
+															helptextValidation
+															onChange={({target: {value: email}}) => {
+																this.setState({email: email})
+															}}/></div>
+											<div><Textfield floatingLabel="Password" type="password"
+															value={this.state.password}
+															required
+															size="40"
+															useInvalidProp
+															invalid={!this.state.hasMinimumPasswordLength}
+															helptext="Your password must be contain at least 6 letters."
+															helptextValidation
+															onChange={({target: {value: password}}) => {
+																this.setState({password: password})
+															}}
+															onKeyUp={this.verifyPasswordLength}/></div>
+											<div><Textfield floatingLabel="Confirm Password" type="password"
+															required
+															value={this.state.passwordConfirm}
+															size="40"
+															useInvalidProp
+															invalid={!this.state.arePasswordsMatching}
+															helptext={"Passwords do not match."}
+															helptextValidation
+															onChange={({target: {value: passwordConfirm}}) => {
+																this.setState({passwordConfirm: passwordConfirm})
+															}}
+															onKeyUp={this.verifyPasswordMatch}/></div>
+										</CardText>
+										<CardActions>
+											<Button
+												type="submit"
+												raised
+												onClick={this.handleRegistration}
+												disabled={!this.validateRegistrationForm()}>Register
+											</Button>
+											<Button
+												raised
+												onClick={this.handleReset}>Clear
+											</Button>
+										</CardActions>
+										<p className="registration-error-message">{this.state.statusText}</p>
+									</Card>
+								</form>
 							</div>
 						</Cell>
 						<Cell col={4}/>
