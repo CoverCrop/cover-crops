138d0f307c6061162ccdff546a277dafe7a12233
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2018-05-17 11:16:24 -0500

    Display error message in case of invalid login.

--- src/app.messages.js
+++ src/app.messages.js
@@ -3,3 +3,4 @@
 export const genericRegistrationErrorMessage = "An error occurred while trying to register your account. Please try again later.";
 export const userNotActiveMessage = "Registration is successful, but your account is not active yet. " +
 	"Please contact an administrator.";
+export const invalidLoginCredentials = "We do not recognize your username and/or password. Please try again.";
