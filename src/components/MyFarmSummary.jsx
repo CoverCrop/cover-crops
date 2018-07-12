f1b57d17b636dfb18480d3b74aaf726449fb4bbd
yanzhao3 <yanzhao3@illinois.edu>
2018-07-12 15:31:26 -0500

    add plant date and harvest date

--- src/components/MyFarmSummary.jsx
+++ src/components/MyFarmSummary.jsx
@@ -14,7 +14,8 @@ class MyFarmSummary extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			obj:{},
+			cropobj:{},
+			fieldobj :{}
 		}
 	}
 
@@ -31,27 +32,109 @@ class MyFarmSummary extends Component {
 		}).then(res => res.text())
 			.catch(error => console.error('Error:', error))
 			.then(text => {
+				let cropobj = {};
+				
 				let textlines = text.split('\n');
 
 				let treaments_line_number = findFirstSubstring(textlines, "TREATMENTS");
-				let tmptext = textlines[treaments_line_number+1].replace("TNAME....................", "YEAR CROP");
-				let b = tmptext.split(' ');
-				let crop = textlines[treaments_line_number+2].split(' ').filter( word => word !== "");
-				let CULTIVARS = { "1": "NEWTON", "2":"DEKALB 591", "3": "M GROUP 2", "4": "FALLOW"};
+				// the first year
+                let tmptext = textlines[treaments_line_number+1].replace("TNAME....................", "YEAR CROP");
+                let b = tmptext.split(' ');
+
+                let CULTIVARS = { "1": "NEWTON", "2":"DEKALB 591", "3": "M GROUP 2", "4": "FALLOW"};
 				let exp =  {"CU": CULTIVARS};
-				let obj = {};
-				for (let i = 0; i < b.length; i++) {
-					//or check with: if (b.length > i) { assignment }
-					obj[b[i]] = exp[b[i]]? exp[b[i]][crop[i]]: crop[i];
+				let linenumber = 2; 
+				let crop = textlines[treaments_line_number+linenumber].split(' ').filter( word => word !== "");
+				while (crop.length >0){
+					let obj = {};
+					for (let i = 0; i < b.length; i++) {
+						//or check with: if (b.length > i) { assignment }
+						
+						obj[b[i]] = exp[b[i]]? exp[b[i]][crop[i]]: crop[i];
+					}
+					let objkey = obj["YEAR"] + " " + obj["CROP"];
+	
+						cropobj[objkey] = obj;
+					
+					
+					linenumber = linenumber+1;
+					crop = textlines[treaments_line_number+linenumber].split(' ').filter( word => word !== "");
+				} 
+
+
+
+				let plant_line_number = findFirstSubstring(textlines, "PLANTING DETAILS");
+				linenumber = 2;
+				crop = textlines[plant_line_number+linenumber].split('  ').filter( word => word !== "")
+				while(crop.length > 0){
+					let objkey = crop[crop.length-1]; 
+				
+					let pdate = crop[0].substring(crop[0].length - 3);   
+                   
+                    	cropobj[objkey]["PDATE"] = pdate;
+        
+                    linenumber = linenumber+1;
+					crop = textlines[plant_line_number+linenumber].split('  ').filter( word => word !== "");
 				}
-				this.setState({obj});
+
+
+
+				let harvest_line_number = findFirstSubstring(textlines, "HARVEST DETAILS");
+				linenumber = 2;
+				crop = textlines[harvest_line_number+linenumber].split(' ').filter( word => word !== "");
+				while(crop.length > 0){
+					let objkey = crop[crop.length-2] + " " + crop[crop.length-1];
+				console.log(" pppp " +objkey)
+					let hdate = crop[1].substring(crop[1].length - 3);
+                   
+                    	cropobj[objkey]["HDATE"] = hdate;
+        
+                    linenumber = linenumber+1;
+					crop = textlines[harvest_line_number+linenumber].split(' ').filter( word => word !== "");
+				}
+
+
+
+				let fieldobj = {};
+				let drainage_type ={"DR000": "No drainage", "DR001": "Ditches",  "DR002": "Sub-surface tiles", "DR003": "Surface furrows"}
+				let drainage_index = text.indexOf('DR00');
+				if(drainage_index > 0 ){
+					fieldobj["DRAINAGE"] = drainage_type[text.substring(drainage_index, drainage_index+5)]
+				}
+			
+
+				this.setState({cropobj, fieldobj});
+
 			});
+
+			
 
 	}
 
 	render() {
 
-	    let {obj} = this.state;
+	    let {cropobj, fieldobj} = this.state;
+
+	    let cropComponent = Object.values(cropobj).filter(obj => obj["CROP"] !== "Fallow" && obj["CROP"] !== "Rye").map(obj => 
+							<tr key={obj["YEAR"]}>
+								<td>{obj["YEAR"]}</td>
+								<td>{obj["CROP"]}</td>
+								<td>{obj["CU"]}</td>
+								<td>{obj["PDATE"]}</td>
+								<td>{obj["HDATE"]}</td>
+							</tr>
+							);
+
+	    let covercropComponent = Object.values(cropobj).filter(obj => obj["CROP"] === "Fallow" || obj["CROP"] === "Rye").map(obj => 
+							<tr key={obj["YEAR"]}>
+								<td>{obj["YEAR"]}</td>
+								<td>{obj["CROP"]}</td>
+								<td>{obj["CU"]}</td>
+								<td>{obj["PDATE"]}</td>
+								<td>{obj["HDATE"]}</td>
+							</tr>
+							);
+
 		return (
 
 				<div>
@@ -67,6 +150,25 @@ class MyFarmSummary extends Component {
 
 					</p>
 					<div className="table-header">
+						FIELD PROFILE
+					</div>
+					<div>
+						<table>
+							<thead>
+							<tr>
+								<th>DRAINAGE</th>
+					
+							</tr>
+							</thead>
+							<tbody>
+						<tr>
+							<td>{fieldobj["DRAINAGE"]}</td>
+						</tr>
+							</tbody>
+							</table>
+					</div>
+
+					<div className="table-header">
 						CROP HISTORY
 					</div>
 						<div>
@@ -76,22 +178,37 @@ class MyFarmSummary extends Component {
 								<th>YEAR</th>
 								<th>CROP</th>
 								<th>CULTIVAR</th>
+								<th>PDATE</th>
+								<th>HDATE</th>
 							</tr>
 							</thead>
 							<tbody>
-							<tr>
-								<td>{obj["YEAR"]}</td>
-								<td>{obj["CROP"]}</td>
-								<td>{obj["CU"]}</td>
-							</tr>
+						{cropComponent}
 							</tbody>
+						
 						</table>
 					</div>
 
 					<div className="table-header">
 						COVER CROP HISTORY
 					</div>
-					<div>aa</div>
+					<div>
+							<table>
+							<thead>
+							<tr>
+								<th>YEAR</th>
+								<th>CROP</th>
+								<th>CULTIVAR</th>
+								<th>PDATE</th>
+								<th>HDATE</th>
+							</tr>
+							</thead>
+							<tbody>
+						{covercropComponent}
+							</tbody>
+						
+						</table>
+					</div>
 					</div>
 				</div>
 
