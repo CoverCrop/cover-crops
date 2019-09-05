d564a2cd4acf85d3b11a06d3318707411e2771f5
vnarah2 <vnarah2@illinois.edu>
2019-09-05 13:34:23 -0500

    CCROP-209: Initial prototype of the dashboard with static input data

--- src/components/DashboardResults.jsx
+++ src/components/DashboardResults.jsx
@@ -1,6 +1,16 @@
 import React, {Component} from "react";
 import { connect } from "react-redux";
 
+// import Plot from "react-plotly.js";
+//TODO: Performance took a hit. Bundle size increased from 6 MB to 13 MB
+
+// customizable method: use your own `Plotly` object - Improve performance
+//plotly-basic.min is supposed to have most features we need, we can use it
+// instead and decrease size by 3 MB
+import Plotly from "plotly.js/dist/plotly.min";
+import createPlotlyComponent from "react-plotly.js/factory";
+const Plot = createPlotlyComponent(Plotly);
+
 class DashboardResults extends Component {
 
 	constructor(props) {
@@ -13,10 +23,88 @@ class DashboardResults extends Component {
 	generateChartsHTML() {
 
 		let resultHtml = [];
+		let selectorOptions = {
+			buttons: [{
+				step: "month",
+				stepmode: "backward",
+				count: 1,
+				label: "1m"
+			}, {
+				step: "month",
+				stepmode: "backward",
+				// count: 6,
+				label: "6m"
+			}, {
+				step: "year",
+				stepmode: "todate",
+				count: 1,
+				label: "YTD"
+			// }, {
+			// 	step: "year",
+			// 	stepmode: "backward",
+			// 	count: 1,
+			// 	label: "1y"
+			}, {
+				step: "all",
+			}],
+		};
 
-				resultHtml.push(
+		let biomass = {
+			x: ["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
+			y: [0, 15, 19, 21],
+			name: "Biomass",
+			type: "scatter",
+			mode: "lines",
+			line: {color: "DeepSkyBlue"}
+		};
+
+		let cn = {
+			x: ["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
+			y: [0, 3, 10, 13],
+			name: "C:N",
+			yaxis: "y2",
+			type: "scatter",
+			mode: "lines", //lines+marks
+			line: {color: "Orange"}
+		};
+
+		let data = [biomass, cn];
+
+		let layout = {
+			title: "Cover Crop Patterns",
+			xaxis: {
+				rangeselector: selectorOptions,
+				rangeslider: {borderwidth: 1}
+			},
+			yaxis: {
+				title: "",
+				tickfont: {color: "DeepSkyBlue"},
+				showgrid: false
+			},
+			yaxis2: {
+				title: "",
+				// titlefont: {color: "red"},
+				tickfont: {color: "Orange"},
+				overlaying: "y",
+				side: "right",
+				showgrid: false
+			}
+		};
+
+		// yaxis: {
+		// 	fixedrange: true
+		// }
+
+
+		resultHtml.push(
 					<div>
-						DASHBOARD GOES HERE
+						<Plot
+							data={data}
+							layout={layout}
+							config={{
+								"displayModeBar": false
+							}}
+						/>
 					</div>);
 
 		return resultHtml;
