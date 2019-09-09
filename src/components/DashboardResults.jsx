7820614574a932c6cf96fc9e3ea317dc4b8ca915
vnarah2 <vnarah2@illinois.edu>
2019-09-09 09:25:40 -0500

    CCROP-209: Replaced dummy data with real run data for the graphs

--- src/components/DashboardResults.jsx
+++ src/components/DashboardResults.jsx
@@ -18,9 +18,127 @@ class DashboardResults extends Component {
 		this.generateChartsHTML = this.generateChartsHTML.bind(this);
 	}
 
+	// Generates charts array object containing individual charts and datasets
+	generateCharts(chartArrayTypeName, plantingDate, harvestDate) {
+		let chartDataArray = {};
+		let colorIndex = 0;
+		let minTime = plantingDate.getTime();
+		let maxTime = harvestDate.getTime();
+		let chartOptions = {};
+
+		if (this.props.hasOwnProperty(chartArrayTypeName) && this.props[chartArrayTypeName] !== null) {
+			if(this.props[chartArrayTypeName].hasOwnProperty("charts")){
+				// Iterate over each chart
+				let charts =  this.props[chartArrayTypeName].charts;
+				for (let dataIndex = 0; dataIndex < charts.length; dataIndex++) {
+					let chartRawData = charts[dataIndex];
+
+					let rawDatasets = chartRawData.datasets;
+					let parsedDatasets = [];
+
+					// Iterate over each dataset in the chart
+					for (let datasetIndex = 0; datasetIndex < rawDatasets.length; datasetIndex++) {
+
+						let rawData = rawDatasets[datasetIndex].data;
+						let parsedData = [];
+
+						// Generate data to be added to the dataset
+						for (let dataIndex = 0; dataIndex < rawData.length; dataIndex++) {
+							// TODO: Remove the if condition. Temporary fix till server-side bug is fixed.
+							if (!(chartRawData.title === "Carbon : Nitrogen" && dataIndex === 0)) {
+								parsedData.push({
+									x: new Date(rawData[dataIndex].YEAR, 0, rawData[dataIndex].DOY),
+									y: rawData[dataIndex].value
+								});
+							}
+						}
+
+						// Create dataset object with appropriate options and data
+						let datasetLabel = (chartArrayTypeName === "withCoverCropChartDataArray") ? "w/ Cover Crop" : "w/o Cover Crop";
+						let dataset = {
+							label: datasetLabel,
+
+							data: parsedData
+						};
+
+						parsedDatasets.push(dataset);
+					}
+
+					let chartData = {
+						// TODO: Check with team on this. Having fixed labels vs dynamic labels selected by chart.js based on input data
+						// labels: labelArray,
+						datasets: parsedDatasets
+					};
+
+					// Chart for this variable has been already created
+					if (chartDataArray.hasOwnProperty(chartRawData.variable)) {
+						chartDataArray[chartRawData.variable].chartData.datasets = chartDataArray[chartRawData.variable].chartData.datasets.concat(parsedDatasets);
+					}
+					// Chart for this variable has to be created
+					else {
+						chartDataArray[chartRawData.variable] = {
+							chartData: chartData,
+							chartOptions: chartOptions
+						};
+					}
+				}
+			}
+		}
+		return chartDataArray;
+	}
 
 	// Generate and return charts HTML content
 	generateChartsHTML() {
+
+		let plantingDate = new Date();
+		let harvestDate = new Date();
+		let chartDataArray = {}; // Associative array to store chart data
+		let biomassDates = [];
+		let biomassValues = [];
+		let cnDates = [];
+		let cnValues = [];
+
+		let cnRows = [];
+		let biomassRows = [];
+
+		if (this.props.hasOwnProperty("userInputJson") && this.props["userInputJson"] !== null && this.props["userInputJson"] !== undefined) {
+
+			let plantingYear = this.props["userInputJson"]["year_planting"];
+			let harvestYear = plantingYear + 1;
+			let plantingDOY = this.props["userInputJson"]["doy_planting"];
+			let harvestDOY = this.props["userInputJson"]["doy_harvest"];
+			plantingDate = new Date(plantingYear, 0, plantingDOY);
+			harvestDate = new Date(harvestYear, 0, harvestDOY);
+		}
+
+		chartDataArray = this.generateCharts("withCoverCropChartDataArray", plantingDate, harvestDate); // generate charts for with cover crop case
+		console.log("dashboard array: ");
+		console.log(chartDataArray);
+		for (let key in chartDataArray) {
+			if(key.toString() === "C:N ratio"){
+				cnRows = chartDataArray[key].chartData.datasets[0].data;
+			}
+
+			if(key.toString() === "TWAD"){
+				biomassRows = chartDataArray[key].chartData.datasets[0].data;
+			}
+		}
+
+		console.log(cnRows);
+		console.log(biomassRows);
+
+		cnRows.forEach(function(element) {
+			cnDates.push(element.x);
+			cnValues.push(element.y);
+		});
+
+		biomassRows.forEach(function(element) {
+			biomassDates.push(element.x);
+			biomassValues.push(element.y);
+		});
+
+		//console.log(cnDates);
+
 
 		let resultHtml = [];
 		let selectorOptions = {
@@ -50,8 +168,8 @@ class DashboardResults extends Component {
 		};
 
 		let biomass = {
-			x: ["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
-			y: [0, 15, 19, 21],
+			x: biomassDates,  //["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
+			y: biomassValues, //[0, 15, 19, 21],
 			name: "Biomass",
 			type: "scatter",
 			mode: "lines",
@@ -59,8 +177,8 @@ class DashboardResults extends Component {
 		};
 
 		let cn = {
-			x: ["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
-			y: [0, 3, 10, 13],
+			x: cnDates, //["2019-01-01", "2019-03-01", "2019-06-01", "2019-09-03"],
+			y: cnValues, //[0, 3, 10, 13],
 			name: "C:N",
 			yaxis: "y2",
 			type: "scatter",
