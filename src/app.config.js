8f9f140343dd99a475aee646cf5cb5f1b2bc97a5
vnarah2 <vnarah2@illinois.edu>
2019-11-18 12:54:00 -0600

    CCROP-226: Updated the weather update date

--- src/app.config.js
+++ src/app.config.js
@@ -5,7 +5,7 @@ const devConfig = {
 	semanticServer: "",
 	domain: "localhost",
 	CLUapi: "http://localhost:5000",
-	latestWeatherDate: "2018-11-30",
+	latestWeatherDate: "2019-09-30",
 	defaultCenterLongLat: [-88.2, 40.14],
 	defaultZoom: 10
 };
@@ -17,7 +17,7 @@ const prodConfig = {
 	semanticServer: "",
 	domain: ".ncsa.illinois.edu",
 	CLUapi: "https://fd-postgres.ncsa.illinois.edu",
-	latestWeatherDate: "2018-11-30",
+	latestWeatherDate: "2019-09-30",
 	defaultCenterLongLat: [-88.2, 40.14],
 	defaultZoom: 10
 };
