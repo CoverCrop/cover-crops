78719f997b1b5b03fc4c96e2344863b616cb84aa
vnarah2 <vnarah2@illinois.edu>
2020-07-07 08:50:02 -0500

    CCROP-255: switched the prod and dev config for hideDecompOutputs

--- src/app.config.js
+++ src/app.config.js
@@ -17,7 +17,7 @@ const devConfig = {
 	useCroplandDataLayer: true, // Use Cropland data layer to get crop rotation history
 	keycloak: Keycloak("http://localhost:3000/keycloak.json"),
 	geoServer: "https://fd-geoserver.ncsa.illinois.edu/geoserver/wms",
-	hideDecompOutputs: true
+	hideDecompOutputs: false
 };
 
 const prodConfig = {
@@ -34,7 +34,7 @@ const prodConfig = {
 	useCroplandDataLayer: true, // Use Cropland data layer to get crop rotation history
 	keycloak: Keycloak(`https://${ prodDomain }/keycloak.json`),
 	geoServer: "https://fd-geoserver.ncsa.illinois.edu/geoserver/wms",
-	hideDecompOutputs: false
+	hideDecompOutputs: true
 };
 
 const config = getConfig();
